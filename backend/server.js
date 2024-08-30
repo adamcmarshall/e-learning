7140c05f06ab41cb84053ad95b24318f6820a3e4
Adam Marshall <adammarshall.email@gmail.com>
2024-08-29 22:44:42 -0600

    added backend files

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 // server.js
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("API is running...");
 });
 
 app.listen(port, () => {
